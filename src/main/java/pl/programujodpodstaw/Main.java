bd3c07ac4f93a8aeb566bde2ce70d3896d2d16b7
Radowids <radekgil9@o2.pl>
2025-03-28 21:12:09 +0100

    Updated variables names

--- src/main/java/pl/programujodpodstaw/Main.java
+++ src/main/java/pl/programujodpodstaw/Main.java
@@ -6,31 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj liczbę: (od 1 do 6)");
-        int k = scanner.nextInt();
+        int userNumber = scanner.nextInt();
 
-        int[] integersArray = new int[6];
-        integersArray[0] = 10;
-        integersArray[1] = 5;
-        integersArray[2] = 2;
-        integersArray[3] = 7;
-        integersArray[4] = 8;
-        integersArray[5] = 7;
+        int[] integersArray = {10, 5, 2, 7, 8, 7};
 
         int[] maxSubstringArray;
 
-        int maxValueOfNextKNumbers = 0;
-        if (k != 1) {
-            maxSubstringArray = new int[integersArray.length + 1 - k];
-            for (int i = 0; i <= integersArray.length - k; i++) {
-                for (int j = i + 1; j < k + i; j++) {
+        int maxValueOfSubArray = 0;
+        if (userNumber != 1) {
+            maxSubstringArray = new int[integersArray.length + 1 - userNumber];
+            for (int i = 0; i <= integersArray.length - userNumber; i++) {
+                for (int j = i + 1; j < userNumber + i; j++) {
                     int max = Math.max(integersArray[j - 1], integersArray[j]);
                     if (j == i + 1) {
-                        maxValueOfNextKNumbers = max;
-                    } else if (maxValueOfNextKNumbers < max) {
-                        maxValueOfNextKNumbers = max;
+                        maxValueOfSubArray = max;
+                    } else if (maxValueOfSubArray < max) {
+                        maxValueOfSubArray = max;
                     }
                 }
-                maxSubstringArray[i] = maxValueOfNextKNumbers;
+                maxSubstringArray[i] = maxValueOfSubArray;
             }
         }else {
             maxSubstringArray = new int[integersArray.length];
